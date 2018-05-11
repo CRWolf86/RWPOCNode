0ef7e8443c29741e19118874e476bcefa13c63ae
Cesar Cordova <cesarcordova@Cesars-MacBook-Air.local>
2018-05-11 00:54:13 -0600

    header links fixed

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
-var http = require('http');
-var fs = require('fs');
+var express = require('express');
 
-var server = http.createServer(function(req, res){
-    console.log('request was made: ' + req.url);
-    if (req.url==='/' || req.url ==='/home' || req.url==='/index') {
-        res.writeHead(200,{'Content-Type':'text/html'});        
-        fs.createReadStream(__dirname+'/website/index.html').pipe(res);
-    }else if(req.url==='form'){
-        res.writeHead(200,{'Content-Type':'text/html'});        
-        fs.createReadStream(__dirname+'/website/form.html').pipe(res);
-    }else if (req.url === '/api/ninjas'){
-        var ninjas = [{name: 'Ryu', age: 29},{name: 'Yoshi', age: 32}];
-        res.writeHead(200,{'Content-Type':'application/json'});
-        res.end(JSON.stringify(ninjas));
-    }else{
-        res.writeHead(404,{'Content-Type':'text/html'});
-        fs.createReadStream(__dirname + '/website/404.html').pipe(res);
-    }
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.use('/assets', express.static('assets'));
+
+app.get('/index/:wod', function(req, res){
+    var data={age:29,job:'ninja', hobbies: ['eating','fighting']};
+    res.render('index',{wod: req.params.wod, data: data});
 });
 
-server.listen(3000, '127.0.0.1');
-console.log('yo dawgs, now listening to port 3000');
+app.get('/form', function(req, res){
+    res.render('form');
+});
+
+app.listen(3000);
